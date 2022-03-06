e3411d074a65ecd7810bf409654a4195ea3fca38
leesiyoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2022-03-06 14:00:02 +0900

    #5.17 Selectors

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   // interface fot toDoState, if null toDos will only return emty array
@@ -7,7 +7,21 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
 }
 
+export const categoryState = atom({
+  key: "catgory",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -4,10 +4,12 @@ import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-  /* selector for interface ITodo["category"]
-  const onClick = (newCategory: IToDo["category"]) => {
+  // selector for interface ITodo["category"]
+  /*
+   const onClick = (newCategory: IToDo["category"]) => {
     console.log("goto" + newCategory);
-  };*/
+  };
+  */
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
@@ -15,7 +17,7 @@ function ToDo({ text, category, id }: IToDo) {
     setToDos((oldToDos) => {
       // toDo.id is id from onClick, id is from props sent by ToDoList
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const oldToDo = oldToDos[targetIndex];
+      //const oldToDo = oldToDos[targetIndex];
       const newToDo = { text, id, category: name as any };
       return [
         ...oldToDos.slice(0, targetIndex),
